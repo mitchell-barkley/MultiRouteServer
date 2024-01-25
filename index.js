17a5a309198abe096333ddcca2b4babe22f32e70
mitchell-barkley <138683597+mitchell-barkley@users.noreply.github.com>
2024-01-25 10:07:37 -0330

    Event Logs

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const routes = require('./routes.js');
 
 const EventEmitter = require('events');
 class MyEmitter extends EventEmitter {};
 const myEmitter = new EventEmitter();
-
-const routes = require('./routes.js');
+const port = 3000;
 
 global.DEBUG = true;
 
@@ -22,6 +22,7 @@ myEmitter.on('route', (url) => {
 });
 
 const server = http.createServer((request, response) => {
+
     if(DEBUG) console.log('Request URL', request.url);
     let path = './views/';
     switch (request.url) {
@@ -29,20 +30,25 @@ const server = http.createServer((request, response) => {
         case '/home':
             path += 'index.html';
             myEmitter.emit('route', path);
-            // fetchFile(path);
             routes.indexPage(path, response);
             break;
         case '/about':
             path += 'about.html';
             myEmitter.emit('route', path);
-            // fetchFile(path);
             routes.aboutPage(path, response);
             break;
         case '/contact':
             path += 'contact.html';
             myEmitter.emit('route', path);
-            // fetchFile(path);
             routes.contactPage(path, response);
+            break;
+        case '/events':
+            myEmitter.emit('event', request.url, 'INFO', 'An event route was requested');
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.end('An event route was requested');
+            break;
+        case '/folder':
+            routes.createFolder(request, response);
             break;
         default:
             if(DEBUG) console.log('404 Route')
@@ -50,17 +56,6 @@ const server = http.createServer((request, response) => {
             response.end('<h1>404 Page Not Found</h1>');
             break;
     }
-    // function fetchFile(fileName) {
-    //     fs.readFile(fileName, (error, content) => {
-    //         if (error) {
-    //             response.writeHead(500, { 'Content-Type': 'text/html' });
-    //             response.end('<h1>500 Internal Server Error</h1>');
-    //         } else {
-    //             response.writeHead(200, { 'Content-Type': 'text/html' });
-    //             response.end(content, 'utf-8');
-    //         }
-    //     });
-    // }
 });
 
 server.listen(3000, () => {
--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+const myEmitter = require('./logEvents');
+
 function indexPage(path, response) {
     fetchFile(path, response);
 }
@@ -10,6 +12,22 @@ function aboutPage(path, response) {
 
 function contactPage(path, response) {
     fetchFile(path, response);
+}
+
+function createFolder(request, response) {
+    const folderName = 'newFolder';
+    fs.mkdir(folderName, (error) => {
+        if(error) {
+            console.error(error);
+            myEmitter.emit('event', request.url, 'ERROR', 'A new folder was NOT created');
+            response.writeHead(500, { 'Content-Type': 'text/plain' });
+            response.end('500 Internal Server Error');
+        } else {
+            myEmitter.emit('event', request.url, 'INFO', 'A new folder was created');
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.end('New folder created');
+        }
+    });
 }
 
 function fetchFile(fileName, response) {
